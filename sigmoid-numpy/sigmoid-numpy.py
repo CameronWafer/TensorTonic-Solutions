a280cde26b611fe6a3c0e4683a06928b36e2a9bd
CameronWafer <129799540+CameronWafer@users.noreply.github.com>
2026-09-23 08:08:29 -0700

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -6,4 +6,4 @@ def sigmoid(x: list | float) -> np.ndarray | float:
     """
     # Write code here
     x = np.asarray(x, dtype=float)
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.e**(-x))
